5414b9deb1437cfabab008428b409e3972c5e091
Saurabh Singh <apple@apples-MacBook-Air-s.local>
2020-04-27 19:14:31 +0530

    changes

--- customer/services/loginService.js
+++ customer/services/loginService.js
@@ -4,9 +4,8 @@ const Promise = require('bluebird');
 exports.loginService = (opts) => {
     console.log("fsafsa")
     return new Promise((resolve, reject) => {
-        console.log("saffsa")
         Promise.coroutine(function* () {
-            const query = 'SELECT * FROM customer where email=? AND password=?';
+            const query = 'SELECT * FROM customer WHERE email=? AND password=?';
             var result = yield dbHandler.dbHandlerPromise(query, opts)
             console.log(result)
             resolve(result)
